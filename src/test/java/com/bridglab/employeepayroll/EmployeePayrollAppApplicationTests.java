a2a6f7cf0a43c4b92884d0b7e263b6ca04098d83
snehak2299 <skanase2299@gmail.com>
2021-09-27 22:04:48 +0530

    [sneha]:basic set up added

---
+++ src/test/java/com/bridglab/employeepayroll/EmployeePayrollAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridglab.employeepayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeePayrollAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
