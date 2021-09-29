9b1e58a966fbc947dbceaf334881ce87ee970f97
snehak2299 <skanase2299@gmail.com>
2021-09-29 18:52:08 +0530

    [sneha]:added validation for name and salary

--- src/main/java/com/bridglab/employeepayroll/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridglab/employeepayroll/dto/EmployeePayrollDTO.java
@@ -1,7 +1,14 @@
 package com.bridglab.employeepayroll.dto;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Pattern;
+
 public class EmployeePayrollDTO {
+	
+	@Pattern(regexp="^[A-Z]{1}[a-zA-Z\\s]{2,}$",message="Employee name Invalid") //validation added for name
 	public String name;
+
+	@Min(value=500,message="Min wage should be more than 500") //validation for salary added
 	public long salary;
 
 	public EmployeePayrollDTO(String name, long salary)  {
